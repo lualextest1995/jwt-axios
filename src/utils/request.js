f421ea40e5c8a110bac6d5ecd61d3edc3e032991
Alex <z0809777@gmail.com>
2025-07-05 16:43:04 +0800

    v2

--- src/utils/request.js
+++ src/utils/request.js
@@ -8,7 +8,6 @@ export const TOKEN_CONST = Object.freeze({
   REFRESH_HEADER_KEY: 'x-refresh-token',
   LS_ACCESS_KEY: 'access_token',
   LS_REFRESH_KEY: 'refresh_token',
-  EXPIRE_SOON_SEC: 60, // 秒；1 分鐘
 })
 
 const queue = new Queue()
@@ -18,14 +17,15 @@ let isRefreshing = false
 const instance = axios.create({
   baseURL: 'http://localhost:3000',
   timeout: 1000,
-  headers: {},
 })
 
 instance.interceptors.request.use(
   (config) => {
     console.log('request config:', config.url)
     const accessToken = Cookie.get(TOKEN_CONST.LS_ACCESS_KEY)
-    config.headers[TOKEN_CONST.ACCESS_HEADER_KEY] = accessToken
+    if(accessToken){
+      config.headers[TOKEN_CONST.ACCESS_HEADER_KEY] = accessToken
+    }
     return config
   },
   (error) => {
@@ -43,41 +43,7 @@ instance.interceptors.response.use(
     console.log('response error:', error)
     const { config, response } = error
     if (response && response.status === 401) {
-      if (!isRefreshing) {
-        isRefreshing = true
-        return refreshAccessToken()
-          .then((res) => {
-            updateToken(res)
-            console.log('處理當前請求:', config.url)
-            instance.request(config)
-            queue.toArray().forEach((item) => {
-              console.log('處理佇列中的請求:', item.config.url)
-              instance.request(item.config).then(item.resolve).catch(item.reject)
-            })
-            return queue.clear() // 清空佇列
-          })
-          .catch((err) => {
-            // 清空佇列與cookie
-            if (err.status === 401) {
-              console.log('清空佇列與cookie', err)
-              queue.clear()
-              Cookie.clear()
-            }
-            return Promise.reject(err)
-          })
-          .finally(() => {
-            isRefreshing = false
-          })
-      }
-
-      return new Promise((resolve, reject) => {
-        console.log('將請求加入佇列:', config.url)
-        queue.enqueue({ config, resolve, reject })
-        console.log(
-          '當前佇列:',
-          queue.toArray().map((item) => item.config.url),
-        )
-      })
+      return HandlerBy401(config)
     }
     return Promise.reject(error)
   },
@@ -97,6 +63,44 @@ async function refreshAccessToken() {
       },
     },
   )
+}
+
+function HandlerBy401(config) {
+  if (!isRefreshing) {
+    isRefreshing = true
+    return refreshAccessToken()
+      .then((res) => {
+        updateToken(res)
+        console.log('處理當前請求:', config.url)
+        instance.request(config)
+        queue.toArray().forEach((item) => {
+          console.log('處理佇列中的請求:', item.config.url)
+          instance.request(item.config).then(item.resolve).catch(item.reject)
+        })
+        return queue.clear() // 清空佇列
+      })
+      .catch((err) => {
+        // 清空佇列與cookie
+        if (err.status === 401) {
+          console.log('清空佇列與cookie', err)
+          queue.clear()
+          Cookie.clear()
+        }
+        return Promise.reject(err)
+      })
+      .finally(() => {
+        isRefreshing = false
+      })
+  }
+
+  return new Promise((resolve, reject) => {
+    console.log('將請求加入佇列:', config.url)
+    queue.enqueue({ config, resolve, reject })
+    console.log(
+      '當前佇列:',
+      queue.toArray().map((item) => item.config.url),
+    )
+  })
 }
 
 function updateToken(response) {
